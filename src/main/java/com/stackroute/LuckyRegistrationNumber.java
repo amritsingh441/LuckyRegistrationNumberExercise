2e733e67251044b84acaef173cb00c26431a2b69
ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-01-22 12:46:28 +0000

    refactored code to pass hobbes failed test cases

--- src/main/java/com/stackroute/LuckyRegistrationNumber.java
+++ src/main/java/com/stackroute/LuckyRegistrationNumber.java
@@ -11,23 +11,28 @@ public class LuckyRegistrationNumber {
 	public static int checkRegistrationNumber(String regNumber) {
 		String pattern = "^(KA|DL)(0[1-9]|10)[A-Z]{1,2}[1-9]\\d{3}$";
 		Pattern ptr = Pattern.compile(pattern);
-		Matcher mtchr = ptr.matcher(regNumber);
-		int n = 0, sum = 0;
-		if(mtchr.matches()) {
-			int tmp = Integer.parseInt(regNumber.substring(regNumber.length()-4));
-			while(tmp > 0) {
-				n = tmp%10;
-				sum = sum + n;
-				tmp = tmp/10;
-			}
-			if(sum%9 == 6) {
-				return 1;
+		if(regNumber!=null) {
+			Matcher mtchr = ptr.matcher(regNumber);
+			int n = 0, sum = 0;
+			if(mtchr.matches()) {
+				int tmp = Integer.parseInt(regNumber.substring(regNumber.length()-4));
+				while(tmp > 0) {
+					n = tmp%10;
+					sum = sum + n;
+					tmp = tmp/10;
+				}
+				if(sum%9 == 6) {
+					return 1;
+				}else {
+					return 0;
+				}
 			}else {
-				return 0;
+				return -1;
 			}
 		}else {
 			return -1;
 		}
+		
 	}
 
 	//Do not print anything other than what's expected/asked in problem
